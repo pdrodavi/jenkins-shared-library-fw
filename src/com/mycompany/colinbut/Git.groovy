3f8aa50780555017f50831a3e0edb1cdab48d957
Pedro Davi <pedrodaviti@gmail.com>
2023-02-18 00:15:11 -0300

    Update Git.groovy

--- src/com/mycompany/colinbut/Git.groovy
+++ src/com/mycompany/colinbut/Git.groovy
@@ -9,7 +9,7 @@ class Git implements Serializable {
     }
 
     def checkout(String repo) {
-        this.script.git credentialsId: Constants.JENKINS_GITHUB_CREDENTIALS_ID, url: "https://github.com/pdrodavi/${repo}.git"
+        this.script.git branch: "main", credentialsId: Constants.JENKINS_GITHUB_CREDENTIALS_ID, url: "https://github.com/pdrodavi/${repo}.git"
     }
 
     String commitHash() {
