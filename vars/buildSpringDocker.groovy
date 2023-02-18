10356a002ece8cbb070488d45ad7b0d2b5ea43b3
Pedro Davi <pedrodaviti@gmail.com>
2023-02-18 00:40:45 -0300

    Update buildSpringDocker.groovy

--- vars/buildSpringDocker.groovy
+++ vars/buildSpringDocker.groovy
@@ -3,63 +3,45 @@ import com.mycompany.colinbut.DockerEcr
 
 def call(Map args) {
     node {
-        
-        stages {
-        
-            stage("Checkout") {
-          
-            inputBranch = input([
-                message: 'Input branch',
-                parameters: [
-                    string(name: 'Input branch')
-                ]
-            ])
-
-            echo "Branch selecionada: ${inputBranch}"
-            echo "Clonando repositório: https://github.com/pdrodavi/app-job-deploy-sb.git"
-          
-            git(branch: "${inputBranch}", Constants.JENKINS_GITHUB_CREDENTIALS_ID, url: "https://github.com/pdrodavi/app-job-deploy-sb.git")
-          
-            }
-
-            stage("Compile") {
-                sh "./mvnw clean compile"
-            }
-
-            stage("Unit Test") {
-                sh "./mvnw test"
-            }
-
-            stage("Integration Test") {
-                sh "./mvnw verify"
-            }
-
-            stage("Static Code Analysis: Sonar") {
-                echo "Running static code analysis using Sonar"
-                withSonarQubeEnv(credentialsId: Constants.SONARQUBE_CREDENTIALS_ID, installationName: Constants.SONARQUBE_INSTALLATION_NAME) {
-                        sh './mvnw sonar:sonar'
-                }
-            }
-
-            stage("Package Artifact Jar") {
-                sh "./mvnw package -DskipTests=true"
-            }
-
-            def dockerEcr = new DockerEcr(this)
-
-            stage("Build Docker Image") {
-                dockerEcr.buildDockerImage("${args.microserviceName}")
-            }
-
-            stage("Publish Docker Image") {
-                dockerEcr.publishDockerImageToECR("${args.microserviceName}")
-            }
-
-            stage("Deploying to Dev") {
-                echo "Deploying to Dev environment"
-            }
-        
+        stage("Checkout") {
+            git branch: "${args.branch}", credentialsId: Constants.JENKINS_GITHUB_CREDENTIALS_ID, url: "https://github.com/pdrodavi/${args.repo}.git"
         }
-        
+
+        stage("Compile") {
+            sh "./mvnw clean compile"
+        }
+
+        stage("Unit Test") {
+            sh "./mvnw test"
+        }
+
+        stage("Integration Test") {
+            sh "./mvnw verify"
+        }
+
+        stage("Static Code Analysis: Sonar") {
+            echo "Running static code analysis using Sonar"
+            withSonarQubeEnv(credentialsId: Constants.SONARQUBE_CREDENTIALS_ID, installationName: Constants.SONARQUBE_INSTALLATION_NAME) {
+                    sh './mvnw sonar:sonar'
+            }
+        }
+
+        stage("Package Artifact Jar") {
+            sh "./mvnw package -DskipTests=true"
+        }
+
+        def dockerEcr = new DockerEcr(this)
+
+        stage("Build Docker Image") {
+            dockerEcr.buildDockerImage("${args.microserviceName}")
+        }
+
+        stage("Publish Docker Image") {
+            dockerEcr.publishDockerImageToECR("${args.microserviceName}")
+        }
+
+        stage("Deploying to Dev") {
+            echo "Deploying to Dev environment"
+        }
     }
 }
