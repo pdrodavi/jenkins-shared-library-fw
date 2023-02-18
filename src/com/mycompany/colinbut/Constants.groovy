9597d27c9942b05f3e6a2caf99fe6c62528a8a23
Pedro Davi <pedrodaviti@gmail.com>
2023-02-18 00:08:00 -0300

    Update Constants.groovy

--- src/com/mycompany/colinbut/Constants.groovy
+++ src/com/mycompany/colinbut/Constants.groovy
@@ -4,7 +4,7 @@ class Constants {
     public static final String SLACK_MESSAGE_HEADER = "Sending Slack Notification"
     public static final String EMAIL_MESSAGE_HEADER = "Sending Email"
 
-    public static final String JENKINS_GITHUB_CREDENTIALS_ID = "github_credentials"
+    public static final String JENKINS_GITHUB_CREDENTIALS_ID = "github-token"
 
     public static final String AWSCLI_CHECK_VERSION_COMMAND = "aws --version"
 
