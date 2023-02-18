fce85492ebdd7e12aa8e04aba58e961ff2b99644
Pedro Davi <pedrodaviti@gmail.com>
2023-02-18 00:33:44 -0300

    Update buildSpringDocker.groovy

--- vars/buildSpringDocker.groovy
+++ vars/buildSpringDocker.groovy
@@ -3,7 +3,10 @@ import com.mycompany.colinbut.DockerEcr
 
 def call(Map args) {
     node {
-        stage("Checkout") {
+        
+        stages {
+        
+            stage("Checkout") {
           
             inputBranch = input([
                 message: 'Input branch',
@@ -17,43 +20,46 @@ def call(Map args) {
           
             git(branch: "${inputBranch}", Constants.JENKINS_GITHUB_CREDENTIALS_ID, url: "https://github.com/pdrodavi/app-job-deploy-sb.git")
           
-        }
-
-        stage("Compile") {
-            sh "./mvnw clean compile"
-        }
-
-        stage("Unit Test") {
-            sh "./mvnw test"
-        }
-
-        stage("Integration Test") {
-            sh "./mvnw verify"
-        }
-
-        stage("Static Code Analysis: Sonar") {
-            echo "Running static code analysis using Sonar"
-            withSonarQubeEnv(credentialsId: Constants.SONARQUBE_CREDENTIALS_ID, installationName: Constants.SONARQUBE_INSTALLATION_NAME) {
-                    sh './mvnw sonar:sonar'
             }
-        }
 
-        stage("Package Artifact Jar") {
-            sh "./mvnw package -DskipTests=true"
-        }
+            stage("Compile") {
+                sh "./mvnw clean compile"
+            }
 
-        def dockerEcr = new DockerEcr(this)
+            stage("Unit Test") {
+                sh "./mvnw test"
+            }
 
-        stage("Build Docker Image") {
-            dockerEcr.buildDockerImage("${args.microserviceName}")
-        }
+            stage("Integration Test") {
+                sh "./mvnw verify"
+            }
 
-        stage("Publish Docker Image") {
-            dockerEcr.publishDockerImageToECR("${args.microserviceName}")
-        }
+            stage("Static Code Analysis: Sonar") {
+                echo "Running static code analysis using Sonar"
+                withSonarQubeEnv(credentialsId: Constants.SONARQUBE_CREDENTIALS_ID, installationName: Constants.SONARQUBE_INSTALLATION_NAME) {
+                        sh './mvnw sonar:sonar'
+                }
+            }
 
-        stage("Deploying to Dev") {
-            echo "Deploying to Dev environment"
+            stage("Package Artifact Jar") {
+                sh "./mvnw package -DskipTests=true"
+            }
+
+            def dockerEcr = new DockerEcr(this)
+
+            stage("Build Docker Image") {
+                dockerEcr.buildDockerImage("${args.microserviceName}")
+            }
+
+            stage("Publish Docker Image") {
+                dockerEcr.publishDockerImageToECR("${args.microserviceName}")
+            }
+
+            stage("Deploying to Dev") {
+                echo "Deploying to Dev environment"
+            }
+        
         }
+        
     }
 }
